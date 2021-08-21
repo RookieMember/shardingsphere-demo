44e8ec3765de611befc2289c2f9ed58390863c99
wangkangpeng <kangpeng.wang@beantechs.com>
2021-08-21 22:13:14 +0800

    添加swagger依赖及接口文档

--- shardingjdbc-demo/src/main/java/com/wkp/shardingjdbc/controller/AddressController.java
+++ shardingjdbc-demo/src/main/java/com/wkp/shardingjdbc/controller/AddressController.java
@@ -2,6 +2,8 @@ package com.wkp.shardingjdbc.controller;
 
 import com.wkp.shardingjdbc.entity.Address;
 import com.wkp.shardingjdbc.service.AddressServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,38 +19,45 @@ import java.util.List;
  */
 @Slf4j
 @RestController
+@Api(value = "广播表相关接口", tags = {"广播表相关接口"})
 public class AddressController {
 
     @Autowired
     AddressServiceImpl addressServiceImpl;
 
     @GetMapping("/confirm_address")
+    @ApiOperation(value = "创建地址")
     public String confirmAddress(long sequenceId){
         long id = addressServiceImpl.addAddress(sequenceId);
         return "创建地址成功:地址ID = " + id;
     }
 
     @GetMapping("/delete_address")
+    @ApiOperation(value = "删除地址")
     public String deleteAddress(long sequenceId){
         return addressServiceImpl.deleteAddress(sequenceId);
     }
 
     @GetMapping("/update_address")
+    @ApiOperation(value = "更新地址")
     public int updateAddress(long sequenceId, String addressName){
         return addressServiceImpl.updateAddress(sequenceId,addressName);
     }
 
     @GetMapping("/select_all_address")
+    @ApiOperation(value = "查询所有地址")
     public List<Address> selectAll() throws SQLException {
         return addressServiceImpl.selectAll();
     }
 
     @GetMapping("/select_range_address")
+    @ApiOperation(value = "按照范围查询地址")
     public List<Address> selectRange(int start,int end) throws SQLException {
         return addressServiceImpl.selectAddressRange(start,end);
     }
 
     @GetMapping("/select_page_address")
+    @ApiOperation(value = "分页查询地址")
     public List<Address> selectPage(int offset,int size) throws SQLException {
         return addressServiceImpl.selectAddressPage(offset,size);
     }
--- shardingjdbc-demo/src/main/java/com/wkp/shardingjdbc/controller/ShardingController.java
+++ shardingjdbc-demo/src/main/java/com/wkp/shardingjdbc/controller/ShardingController.java
@@ -2,6 +2,8 @@ package com.wkp.shardingjdbc.controller;
 
 import com.wkp.shardingjdbc.dto.OrderInfoDto;
 import com.wkp.shardingjdbc.service.OrderServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@Api(value = "分库分表相关接口", tags = {"分库分表相关接口"})
 public class ShardingController {
 
     @Autowired
     OrderServiceImpl orderService;
 
     @GetMapping("/confirm_order")
+    @ApiOperation(value = "创建订单")
     public String confirmOrder(int sequenceId){
         long id = orderService.confirmOrder(sequenceId);
         return "创建订单成功:订单ID = " + id;
     }
 
     @GetMapping("/order_histroy_list")
+    @ApiOperation(value = "查询所有订单")
     public OrderInfoDto orderHistoryList(){
         return orderService.selectAll();
     }
@@ -31,6 +36,7 @@ public class ShardingController {
      * @return
      */
     @GetMapping("/delete_histroy_order")
+    @ApiOperation(value = "删除订单")
     public String deleteHistroyOrder(long orderId){
         return orderService.deleteData(orderId);
     }
@@ -42,6 +48,7 @@ public class ShardingController {
      * @return
      */
     @GetMapping("/update_histroy_order")
+    @ApiOperation(value = "修改订单")
     public int updateHistoryOrderStatus(long orderId,String status){
         return orderService.updateOrder(orderId,status);
     }
@@ -53,6 +60,7 @@ public class ShardingController {
      * @return
      */
     @GetMapping("/order_range_list")
+    @ApiOperation(value = "订单范围查询")
     public OrderInfoDto orderRangeList(long start,long end){
         return orderService.selectOrderRange(start,end);
     }
@@ -64,6 +72,7 @@ public class ShardingController {
      * @return
      */
     @GetMapping("/item_range_list")
+    @ApiOperation(value = "商品范围查询")
     public OrderInfoDto orderItemRangeList(int start,int end){
         return orderService.selectOrderItemRange(start,end);
     }
@@ -75,11 +84,13 @@ public class ShardingController {
      * @return
      */
     @GetMapping("/item_range_in_list")
+    @ApiOperation(value = "关联表查询笛卡尔积")
     public OrderInfoDto orderItemRangeInList(long start,long end){
         return orderService.selectOrderItemWithIn(start,end);
     }
 
     @GetMapping("/item_page_list")
+    @ApiOperation(value = "订单分页查询")
     public OrderInfoDto orderPageList(long offset,long size){
         return orderService.selectOrderPageList(offset,size);
     }
